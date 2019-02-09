853ea5f840bca2bad33ddf061219e0955fa1c58e
mehmet gürdal <mgurdal@protonmail.com>
2019-02-10 00:20:05 +0300

    add generate_jwt unit test

---
+++ test/test_generate_jwt.py
@@ -0,0 +1,31 @@
+import jwt
+from aiohttp import web
+from aiohttp.test_utils import make_mocked_request
+from aiohttp_auth import auth
+
+
+async def test_generate_jwt_adds_expiration_date():
+
+    app = web.Application()
+    app['aiohttp_auth'] = auth.JWTAuth(
+        jwt_secret='test_secret',
+        duration=259200,
+        jwt_algorithm='HS256'
+    )
+
+    stub_request = make_mocked_request(
+        'GET', '/', app=app
+    )
+
+    payload = {'user_id': 4}
+    token = await auth.generate_jwt(
+        stub_request, payload
+    )
+
+    jwt_payload = jwt.decode(
+        token,
+        'test_secret',
+        algorithms=['HS256']
+    )
+
+    assert 'exp' in jwt_payload
