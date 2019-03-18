bec1e34c0d5e738973648bbe1e4642a2aa0941f2
mehmet gürdal <mgurdal@protonmail.com>
2019-03-19 00:07:25 +0300

    add default matching algorithms

---
+++ aiohttp_auth/matching_algorithms.py
@@ -0,0 +1,31 @@
+from typing import Iterable, Hashable
+
+
+def match_any(
+        required: Iterable[Hashable], provided: Iterable[Hashable])-> bool:
+    required_scopes = set(required)
+    provided_scopes = set(provided)
+
+    scopes_matches = required_scopes.intersection(provided_scopes) != set()
+
+    return scopes_matches
+
+
+def match_exact(
+        required: Iterable[Hashable], provided: Iterable[Hashable]) -> bool:
+    required_scopes = set(required)
+    provided_scopes = set(provided)
+
+    scopes_matches = required_scopes == provided_scopes
+
+    return scopes_matches
+
+
+def match_all(
+        required: Iterable[Hashable], provided: Iterable[Hashable]) -> bool:
+    required_scopes = set(required)
+    provided_scopes = set(provided)
+
+    scopes_matches = required_scopes.issubset(provided_scopes)
+
+    return scopes_matches
