085ad48ae7e3c2adada38f48d27fa1d2b626052a
mehmet gürdal <mgurdal@protonmail.com>
2019-03-28 00:33:26 +0300

    add authenticator setup tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,10 +1,28 @@
+import pytest
 from aiohttp import web
-from aiohttp_auth import auth
+from aiohttp_auth.authenticators.jwt import JWTAuth
+
+
+async def test_setup_requires_authenticate_method():
+    app = web.Application()
+
+    class TestAuth(JWTAuth):
+        pass
+
+    with pytest.raises(TypeError) as err:
+        TestAuth.setup(app)
+
+    assert err
 
 
 async def test_setup_injects_jwt_auth():
     app = web.Application()
 
-    initialized_app = auth.setup(app, authenticator=None, jwt_secret='tests')
+    class TestAuth(JWTAuth):
 
-    assert 'aiohttp_auth' in initialized_app
+        async def authenticate(self, request: web.Request):
+            pass
+
+    TestAuth.setup(app)
+
+    assert 'aiohttp_auth' in app
