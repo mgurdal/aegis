b9715712544bb5401a90ceebbed07f3bfb80bce3
mehmet gürdal <mgurdal@protonmail.com>
2019-03-28 00:24:20 +0300

    add BaseAuthenticator

---
+++ aiohttp_auth/authenticators/base.py
@@ -0,0 +1,70 @@
+import inspect
+from abc import ABCMeta, abstractmethod
+from typing import Callable, Hashable, Iterable, Union
+
+from aiohttp import web
+
+from ..matching_algorithms import match_all, match_any, match_exact
+from ..middlewares import auth_middleware
+from ..routes import make_auth_route, make_me_route
+
+
+class BaseAuthenticator(metaclass=ABCMeta):
+    type: str
+
+    @staticmethod
+    async def check_permissions(
+            user_scopes: Iterable[Hashable],
+            required_scopes: Iterable[Hashable],
+            algorithm: Union[str, Callable] = 'any'
+    ) -> bool:
+        # user tries to reach to a scoped end-point
+        if algorithm == 'any':
+            has_permission = match_any(
+                required=required_scopes,
+                provided=user_scopes
+            )
+        elif algorithm == 'all':
+            has_permission = match_all(
+                required=required_scopes,
+                provided=user_scopes
+            )
+        elif algorithm == 'exact':
+            has_permission = match_exact(
+                required=required_scopes,
+                provided=user_scopes
+            )
+        elif inspect.isfunction(algorithm):
+            has_permission = algorithm(
+                required=required_scopes,
+                provided=user_scopes
+            )
+        else:
+            raise TypeError(
+                "Invalid algorithm type. "
+                "Options 'all', 'any', 'exact', callable"
+            )
+        return has_permission
+
+    @abstractmethod
+    async def decode(self, token: str) -> dict:
+        """Takes token and returns the user information as dict"""
+
+    @abstractmethod
+    async def authenticate(self, request: web.Request):
+        """Returns JSON serializable user"""
+
+    @abstractmethod
+    async def get_scopes(self, request: web.Request):
+        """Takes returns user's permissions"""
+
+    @classmethod
+    def setup(cls, app, name='aiohttp_auth'):
+        app.middlewares.append(auth_middleware)
+        authenticator = cls()
+        print(authenticator)
+        auth_route = make_auth_route(authenticator)
+        me_route = make_me_route()
+        app.router.add_post('/auth', auth_route)
+        app.router.add_get('/me', me_route)
+        app[name] = authenticator
---
+++ tests/test_base.py
@@ -0,0 +1,112 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+from aiohttp_auth.authenticators.base import BaseAuthenticator
+
+
+async def test_check_permissions_returns_uses_any_match_as_default():
+    user_permissions = ('super_user',)
+
+    required_permissions = ('super_user',)
+    with patch('aiohttp_auth.authenticators.base.match_any') as match_any:
+        has_permissions = await BaseAuthenticator.check_permissions(
+            user_permissions, required_permissions
+        )
+
+        assert has_permissions
+        match_any.assert_called_once_with(
+            required=required_permissions,
+            provided=user_permissions
+        )
+
+
+async def test_check_permissions_returns_with_calls_any_match_algorithm():
+    user_permissions = ('regular_user',)
+
+    required_permissions = ('super_user', 'regular_user')
+    with patch('aiohttp_auth.authenticators.base.match_any') as match_any:
+        has_permissions = await BaseAuthenticator.check_permissions(
+            user_permissions, required_permissions, algorithm='any'
+        )
+
+        assert has_permissions
+        match_any.assert_called_once_with(
+            required=required_permissions,
+            provided=user_permissions
+        )
+
+
+async def test_check_permissions_returns_calls_all_match_algorithm():
+    user_permissions = ('regular_user',)
+
+    required_permissions = ('super_user', 'regular_user')
+
+    with patch('aiohttp_auth.authenticators.base.match_all') as match_all:
+        match_all.return_value = True
+        has_permissions = await BaseAuthenticator.check_permissions(
+            user_permissions, required_permissions, algorithm='all'
+        )
+
+        assert has_permissions
+        match_all.assert_called_once_with(
+            required=required_permissions,
+            provided=user_permissions
+        )
+
+
+async def test_check_permissions_returns_calls_exact_match_algorithm():
+    user_permissions = ('regular_user',)
+
+    required_permissions = ('super_user', 'regular_user')
+
+    with patch('aiohttp_auth.authenticators.base.match_exact') as match_exact:
+        match_exact.return_value = True
+        has_permissions = await BaseAuthenticator.check_permissions(
+            user_permissions, required_permissions, algorithm='exact'
+        )
+
+        assert has_permissions
+        match_exact.assert_called_once_with(
+            required=required_permissions,
+            provided=user_permissions
+        )
+
+
+async def test_check_permissions_returns_calls_custom_algorithm():
+    user_permissions = ('regular_user',)
+
+    required_permissions = ('super_user', 'regular_user')
+
+    custom_mock = MagicMock(return_value=True)
+
+    def custom_algorithm(*args, **kwargs):
+        return custom_mock(*args, **kwargs)
+
+    has_permissions = await BaseAuthenticator.check_permissions(
+        user_permissions, required_permissions, algorithm=custom_algorithm
+    )
+
+    assert has_permissions
+    custom_mock.assert_called_once_with(
+        required=required_permissions,
+        provided=user_permissions
+    )
+
+
+async def test_check_permissions_handles_invalid_algorithm():
+
+    user_permissions = ('regular_user',)
+
+    required_permissions = ('super_user', 'regular_user')
+
+    invalid_algorithm = object()
+
+    with pytest.raises(TypeError) as te:
+        # noinspection PyTypeChecker
+        await BaseAuthenticator.check_permissions(
+            user_permissions, required_permissions, algorithm=invalid_algorithm
+        )
+
+    assert str(te.value) == (
+        "Invalid algorithm type. Options 'all', 'any', 'exact', callable"
+    )
