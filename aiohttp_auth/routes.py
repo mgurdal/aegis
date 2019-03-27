d5ea323bd2d53a83e6bafbdb1f4373227cff681a
mehmet gürdal <mgurdal@protonmail.com>
2019-03-28 00:30:07 +0300

    update routes

--- aiohttp_auth/routes.py
+++ aiohttp_auth/routes.py
@@ -1,22 +1,22 @@
 from aiohttp import web
 
-from .exceptions import UserDefinedException
-from .responses import access_token, error_response, forbidden
-from .tokenizers import generate_jwt
+from .exceptions import AuthException, ForbiddenException
 
 
 def make_auth_route(authenticator):
     async def auth_route(request: web.Request):
         try:
-            user = await authenticator(request)
-        except UserDefinedException as ude:
-            return error_response(request, ude)
+            user = await authenticator.authenticate(request)
+        except AuthException as ae:
+            return ae.make_response(request)
 
         # use auth.login to generate a JWT token
         # with some unique user information
-        token = await generate_jwt(request, user)
+        token = await authenticator.encode(user)
 
-        return access_token(token)
+        return web.json_response({
+            "access_token": token
+        }, status=200)
 
     return auth_route
 
@@ -25,11 +25,8 @@ def make_me_route():
     async def me_route(request: web.Request):
         user_authenticated = hasattr(request, 'user')
         if user_authenticated:
-            user = request.user
-            # remove expiration date
-            user.pop('exp')
             return web.json_response(request.user)
         else:
-            return forbidden(request)
+            return ForbiddenException.make_response(request)
 
     return me_route
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,27 +1,10 @@
 import json
 
+from aiohttp import web
 from aiohttp.test_utils import make_mocked_request
-from aiohttp_auth.exceptions import UserDefinedException
+from aiohttp_auth.exceptions import AuthException
 from aiohttp_auth.routes import make_auth_route, make_me_route
 from asynctest import CoroutineMock, patch
-
-
-async def test_auth_route_awaits_dependencies():
-
-    stub_request = make_mocked_request('GET', '/')
-
-    stub_user = {'user_id': 1}
-    authenticator = CoroutineMock(return_value=stub_user)
-    auth_route = make_auth_route(authenticator)
-
-    with patch('aiohttp_auth.routes.generate_jwt') as mocked_generate_jwt:
-        mocked_generate_jwt.return_value = 'test_token'
-        await auth_route(stub_request)
-
-        assert authenticator.awaited_once_with(stub_request)
-        assert mocked_generate_jwt.awaited_once_with(
-            stub_request, stub_user
-        )
 
 
 async def test_auth_route_returns_access_token():
@@ -29,42 +12,41 @@ async def test_auth_route_returns_access_token():
     stub_request = make_mocked_request('GET', '/')
 
     stub_user = {'user_id': 1}
-    authenticator = CoroutineMock(return_value=stub_user)
+    authenticator = CoroutineMock()
+    authenticator.authenticate = CoroutineMock(return_value=stub_user)
+    encoder = authenticator.encode = CoroutineMock(return_value="token")
+
     auth_route = make_auth_route(authenticator)
+    token_payload = await auth_route(stub_request)
 
-    with patch('aiohttp_auth.routes.generate_jwt') as mocked_generate_jwt:
-        with patch('aiohttp_auth.routes.access_token') as access_token:
-            access_token.return_value.status = 200
-            access_token.return_value.body = b'{"access_token": "x"}'
-
-            token_response = await auth_route(stub_request)
-            token_payload = json.loads(token_response.body)
-
-            mocked_generate_jwt.assert_called_with(
-                stub_request, stub_user
-            )
-            assert authenticator.awaited_once_with(stub_request)
-            assert token_payload == {
-                'access_token': 'x'
-            }
+    assert encoder.encode.awaited_once_with(stub_user)
+    assert authenticator.authenticate.awaited_once_with(stub_request)
+    assert json.loads(token_payload.body) == {
+        'access_token': 'token'
+    }
 
 
-async def test_auth_route_handles_user_exceptions():
+async def test_auth_route_handles_auth_exception():
 
     stub_request = make_mocked_request('GET', '/')
+    AuthException.make_response = CoroutineMock(
+        return_value=web.json_response({})
+    )
 
-    class TestException(UserDefinedException):
+    class TestException(AuthException):
         status = 400
-        title = "test"
-        detail = "test"
 
-    authenticator = CoroutineMock(side_effect=TestException())
+        @staticmethod
+        def get_schema():
+            return {}
+    authenticator = CoroutineMock()
+    authenticator.authenticate = CoroutineMock(side_effect=TestException())
+
     auth_route = make_auth_route(authenticator)
 
-    with patch('aiohttp_auth.routes.error_response') as error_response:
-        await auth_route(stub_request)
+    await auth_route(stub_request)
 
-        assert error_response.called
+    assert AuthException.make_response.called_once_with(stub_request)
 
 
 async def test_me_route_returns_user_information():
@@ -88,19 +70,17 @@ async def test_me_route_returns_user_information():
 
     assert user_response.status == 200
     assert user_payload == stub_user
-    assert 'exp' not in user_payload
 
 
-async def test_me_route_returns_403_if_user_is_not_authenticated():
+async def test_me_route_uses_forbidden_exception_if_not_authenticated():
     """
     Test me route returns 403, Forbidden if user is not authenticated.
     """
     stub_request = make_mocked_request('GET', '/')
     me_route = make_me_route()
-    with patch('aiohttp_auth.routes.forbidden') as forbidden:
-        forbidden.return_value.status = 403
+    with patch('aiohttp_auth.routes.ForbiddenException.make_response') as fe:
+        fe.return_value.status = 403
 
-        user_response = await me_route(stub_request)
+        await me_route(stub_request)
 
-        assert user_response.status == 403
-        assert forbidden.called
+        assert fe.awaited_once_with(stub_request)
