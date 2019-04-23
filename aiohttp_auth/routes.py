dce7fad52451d976cff43ae26bcc3dcd95324342
mehmet gürdal <mgurdal@protonmail.com>
2019-04-23 03:31:28 +0300

    Add refresh token route

--- aiohttp_auth/routes.py
+++ aiohttp_auth/routes.py
@@ -1,22 +1,30 @@
 from aiohttp import web
 
-from .exceptions import AuthException, ForbiddenException
+from .auth import login_required
+from .exceptions import (AuthException, ForbiddenException,
+                         InvalidRefreshTokenException)
 
 
 def make_auth_route(authenticator):
     async def auth_route(request: web.Request):
         try:
             user = await authenticator.authenticate(request)
+            request.user = user
         except AuthException as ae:
             return ae.make_response(request)
 
         # use auth.login to generate a JWT token
         # with some unique user information
         token = await authenticator.encode(user)
-
-        return web.json_response({
+        token_payload = {
             "access_token": token
-        }, status=200)
+        }
+        if authenticator.refresh_token:
+            token_payload[
+                "refresh_token"] = await authenticator.get_refresh_token(
+                request)
+
+        return web.json_response(token_payload, status=200)
 
     return auth_route
 
@@ -30,3 +38,18 @@ def make_me_route():
             return ForbiddenException.make_response(request)
 
     return me_route
+
+
+def make_refresh_route(authenticator):
+    @login_required
+    async def refresh_route(request: web.Request):
+        refresh_valid = await authenticator.validate_refresh_token(request)
+        if not refresh_valid:
+            return InvalidRefreshTokenException.make_response(request)
+
+        access_token = await authenticator.encode(request.user)
+        return web.json_response({
+            "access_token": access_token
+        })
+
+    return refresh_route
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,18 +1,20 @@
 import json
+from unittest.mock import MagicMock
 
 from aiohttp import web
 from aiohttp.test_utils import make_mocked_request
 from aiohttp_auth.exceptions import AuthException
-from aiohttp_auth.routes import make_auth_route, make_me_route
+from aiohttp_auth.routes import make_auth_route, make_me_route, \
+    make_refresh_route
 from asynctest import CoroutineMock, patch
 
 
-async def test_auth_route_returns_access_token():
-
+async def test_auth_route_only_returns_access_token_if_not_refresh():
     stub_request = make_mocked_request('GET', '/')
 
     stub_user = {'user_id': 1}
     authenticator = CoroutineMock()
+    authenticator.refresh_token = False
     authenticator.authenticate = CoroutineMock(return_value=stub_user)
     encoder = authenticator.encode = CoroutineMock(return_value="token")
 
@@ -26,8 +28,30 @@ async def test_auth_route_returns_access_token():
     }
 
 
-async def test_auth_route_handles_auth_exception():
+async def test_auth_route_returns_access_and_refresh_token():
+    stub_request = make_mocked_request('GET', '/')
 
+    stub_user = {'user_id': 1}
+    refresh_token = "test_refresh"
+    access_token = "test_access"
+    authenticator = CoroutineMock()
+    authenticator.refresh_token = True
+    authenticator.authenticate = CoroutineMock(return_value=stub_user)
+    authenticator.get_refresh_token = CoroutineMock(return_value=refresh_token)
+    encoder = authenticator.encode = CoroutineMock(return_value=access_token)
+
+    auth_route = make_auth_route(authenticator)
+    token_payload = await auth_route(stub_request)
+
+    assert encoder.encode.awaited_once_with(stub_user)
+    assert authenticator.authenticate.awaited_once_with(stub_request)
+    assert json.loads(token_payload.body) == {
+        "access_token": access_token,
+        "refresh_token": refresh_token
+    }
+
+
+async def test_auth_route_handles_auth_exception():
     stub_request = make_mocked_request('GET', '/')
     AuthException.make_response = CoroutineMock(
         return_value=web.json_response({})
@@ -39,6 +63,7 @@ async def test_auth_route_handles_auth_exception():
         @staticmethod
         def get_schema():
             return {}
+
     authenticator = CoroutineMock()
     authenticator.authenticate = CoroutineMock(side_effect=TestException())
 
@@ -84,3 +109,86 @@ async def test_me_route_uses_forbidden_exception_if_not_authenticated():
         await me_route(stub_request)
 
         assert fe.awaited_once_with(stub_request)
+
+
+async def test_auth_route_adds_refresh_token_if_activated():
+    stub_request = make_mocked_request('GET', '/')
+
+    stub_user = {'user_id': 1}
+    authenticator = CoroutineMock()
+    authenticator.authenticate = CoroutineMock(return_value=stub_user)
+    authenticator.refresh_token = True
+    authenticator.encode = CoroutineMock(return_value="token")
+    authenticator.get_refresh_token = CoroutineMock(
+        return_value="refresh_token"
+    )
+
+    auth_route = make_auth_route(authenticator)
+    token_payload = await auth_route(stub_request)
+
+    response_data = json.loads(token_payload.body)
+
+    authenticator.get_refresh_token.assert_called_once()
+    assert "refresh_token" in response_data
+
+
+async def test_refresh_route_renews_access_token():
+    """
+    Validates refresh token with authenticator.validate_refresh_token
+    Calls authenticator.renew_access_token to retrieve a new access token
+    """
+    refresh_payload = {"refresh_token": "test"}
+    access_token = "access_token"
+
+    stub_request = CoroutineMock()
+    stub_request.json = CoroutineMock(return_value=refresh_payload)
+
+    authenticator = CoroutineMock()
+    authenticator.encode = CoroutineMock(
+        return_value=access_token
+    )
+    authenticator.validate_refresh_token = CoroutineMock(
+        return_value=True
+    )
+
+    # bypass login
+    with patch('aiohttp_auth.routes.login_required', lambda x: x):
+        refresh_route = make_refresh_route(authenticator)
+        token_payload = await refresh_route(stub_request)
+
+        response_data = json.loads(token_payload.body)
+
+        assert authenticator.encode.called
+        assert authenticator.validate_refresh_token.called
+        assert "access_token" in response_data
+
+
+async def test_refresh_returns_bad_request_if_refresh_token_invalid():
+    """
+    Validates refresh token with authenticator.validate_refresh_token
+    returns InvalidRefreshTokenException.make_response if invalid
+    """
+    refresh_payload = {"refresh_token": "test"}
+    access_token = "access_token"
+
+    stub_request = CoroutineMock()
+    stub_request.json = CoroutineMock(return_value=refresh_payload)
+
+    authenticator = CoroutineMock()
+    authenticator.renew_access_token = CoroutineMock(
+        return_value=access_token
+    )
+    authenticator.validate_refresh_token = CoroutineMock(
+        return_value=False
+    )
+
+    with patch('aiohttp_auth.routes.InvalidRefreshTokenException') as irte:
+        # bypass login
+        with patch('aiohttp_auth.routes.login_required', lambda x: x):
+            irte.make_response = MagicMock()
+            refresh_route = make_refresh_route(authenticator)
+            await refresh_route(stub_request)
+
+            assert authenticator.validate_refresh_token.called
+            assert not authenticator.encode.called
+            irte.make_response.assert_called_once_with(stub_request)
