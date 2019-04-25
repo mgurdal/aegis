9d3fd01c431dd6af796d6f1d1b1a498c37331221
mehmet gürdal <mgurdal@protonmail.com>
2019-04-25 04:09:25 +0300

    Add basic authentication

---
+++ aiohttp_auth/authenticators/basic.py
@@ -0,0 +1,43 @@
+import base64
+import binascii
+from abc import abstractmethod
+from typing import Dict, Any
+
+from aiohttp import web
+
+from aiohttp_auth.exceptions import InvalidTokenException
+from .base import BaseAuthenticator
+
+
+class BasicAuth(BaseAuthenticator):
+    user_id = "user_id"
+    password = "password"
+    auth_endpoint = None
+    me_endpoint = None
+
+    async def get_scopes(self, request: web.Request):
+        """:returns user's permissions"""
+
+    @abstractmethod
+    async def authenticate(self, request: web.Request) -> Dict[str, Any]:
+        """Returns JSON serializable user"""
+
+    async def decode(self, token: str, verify=True) -> dict:
+        """
+        Decodes basic token and returns user's id and password as a dict.
+        """
+        try:
+            basic_token = token.replace("Basic ", "").encode()
+
+            decoded_credentials = base64.b64decode(
+                basic_token, validate=verify
+            ).decode()
+            user_id, password = decoded_credentials.split(":")
+            # create payload with pre-defined keys
+            payload = {
+                self.user_id: user_id,
+                self.password: password
+            }
+            return payload
+        except binascii.Error:
+            raise InvalidTokenException()
---
+++ tests/test_basic_authenticator.py
@@ -0,0 +1,92 @@
+import binascii
+from unittest.mock import patch
+
+import pytest
+
+from aiohttp_auth.authenticators.basic import BasicAuth
+from aiohttp_auth.exceptions import InvalidTokenException
+
+
+async def test_decode_raises_invalid_token_exception_on_decode_error():
+    with patch('aiohttp_auth.authenticators.basic.base64.b64decode') as decode:
+        decode.side_effect = binascii.Error()
+
+        class TestBasicAuth(BasicAuth):
+
+            async def authenticate(self, request):
+                pass
+
+        auth = TestBasicAuth()
+        invalid_token = "test"
+
+        with pytest.raises(InvalidTokenException) as ex:
+            await auth.decode(invalid_token)
+
+        assert ex is not None
+        assert decode.called
+        assert auth
+
+
+async def test_decode_returns_credentials_with_default_keys():
+    with patch('aiohttp_auth.authenticators.basic.base64.b64decode') as decode:
+        credentials = b"test:test"
+        decode.return_value = credentials
+
+        class TestBasicAuth(BasicAuth):
+
+            async def authenticate(self, request):
+                pass
+
+        auth = TestBasicAuth()
+        token = "test=="
+
+        decoded_credentials = await auth.decode(token)
+        expected_credentials = {
+            "user_id": "test",
+            "password": "test"
+        }
+        assert decoded_credentials == expected_credentials
+
+
+async def test_decode_returns_credentials_with_altered_keys():
+    with patch('aiohttp_auth.authenticators.basic.base64.b64decode') as decode:
+        credentials = b"test:test"
+        decode.return_value = credentials
+
+        class TestBasicAuth(BasicAuth):
+            user_id = "email"
+            password = "password"
+
+            async def authenticate(self, request):
+                pass
+
+        auth = TestBasicAuth()
+        token = "test=="
+
+        decoded_credentials = await auth.decode(token)
+        expected_credentials = {
+            "email": "test",
+            "password": "test"
+        }
+        assert decoded_credentials == expected_credentials
+
+
+async def test_decode_passes_verify_parameter_into_decoder():
+    with patch('aiohttp_auth.authenticators.basic.base64.b64decode') as decode:
+        credentials = b"test:test"
+        decode.return_value = credentials
+
+        class TestBasicAuth(BasicAuth):
+
+            async def authenticate(self, request):
+                pass
+
+        auth = TestBasicAuth()
+        token = "test=="
+
+        await auth.decode(token, verify=False)
+
+        decode.assert_called_once_with(token.encode(), validate=False)
+
+        await auth.decode(token, verify=True)
+        decode.assert_called_with(token.encode(), validate=True)
